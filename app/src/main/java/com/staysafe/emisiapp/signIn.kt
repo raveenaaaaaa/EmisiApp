12a416dd442f2dc45ed2ebd8b3ee4a1ee01986eb
furqanakbarsy <bismillah4350@gmail.com>
2022-01-12 11:03:11 +0700

    Initial commit

---
+++ app/src/main/java/com/staysafe/emisiapp/signIn.kt
@@ -0,0 +1,17 @@
+package com.staysafe.emisiapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class signIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+
+
+}
